f813dd540b29c78c3915fd531e24f0c2c5fe9942
Kalikalyan Dash <kkdgrece@gmail.com>
2018-08-06 00:31:31 -0700

    list layout of flights

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { FlightBookingComponent } from './flight-booking/flight-booking.component';
+import { SideFilterComponent } from './flight-booking/side-filter/side-filter.component';
+import { FlightListComponent } from './flight-booking/flight-list/flight-list.component';
+import { FlightItemComponent } from './flight-booking/flight-list/flight-item/flight-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightBookingComponent,
+    SideFilterComponent,
+    FlightListComponent,
+    FlightItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
